8e314dade25d5ea91518b861215ba200a63a78c8
Kelvin Agbenyo <49683507+sparkwud@users.noreply.github.com>
2021-07-15 20:19:05 +0000

    DOUGHNUT - Harry and big doughnuts

--- doughnut.py
+++ doughnut.py
@@ -5,8 +5,6 @@ for step in range(t):
 
 
 
-
-
 for step in range(int(input("Enter the number of steps: "))):
     c, k, w = [int(input()) for num in range(3)]
     print("yes") if k >= c * w else print("no")
