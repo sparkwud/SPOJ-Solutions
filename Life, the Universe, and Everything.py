156bdd595e25f752852d7deddc9c2201ab7ea216
sparkwud <kelvinagbenyo@gmail.com>
2021-07-15 20:40:50 +0000

    Create Life, the Universe, and Everything.py

---
+++ Life, the Universe, and Everything.py
@@ -0,0 +1,12 @@
+number = int(input())
+while number != 42:
+    print(number)
+    number = int(input())
+    
+
+
+# while True:
+#     number = int(input())
+#     if number == 42:
+#         break
+#     print(number)
